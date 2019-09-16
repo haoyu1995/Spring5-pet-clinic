c6e23f0daef08b7eb685bfead63357595dfd7953
Hedy <Hedy@github.com>
2019-09-16 02:04:30 -0400

    initial project add

---
+++ src/test/java/com/springframe/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,15 @@
+package com.springframe.sfgpetclinic;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
